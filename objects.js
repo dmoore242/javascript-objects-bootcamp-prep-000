9c0f759908b53f57e691504f64834aaa9a77e846
Devin Moore <devin.moore@volken.org>
2017-12-07 02:40:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,7 @@ var playlist = {
   artistName: "song title"
 }
 
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, {
+    [artistName]: songTitle })
+}
